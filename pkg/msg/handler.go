fe6eb78a446a37a76049e11043303604738736ed
barry <kooksee@163.com>
2024-07-01 19:08:43 +0800

    fix: barry 2024-07-01 19:08:43

--- pkg/msg/handler.go
+++ pkg/msg/handler.go
@@ -15,7 +15,7 @@
 package msg
 
 import (
-	"github.com/fatedier/frp/pkg/util/log"
+	"fmt"
 	"io"
 	"reflect"
 )
@@ -70,7 +70,7 @@ func (d *Dispatcher) readLoop() {
 			return
 		}
 
-		log.Debugf("receive message from server: %s, exists: %v", reflect.TypeOf(m), d.msgHandlers[reflect.TypeOf(m)] != nil)
+		fmt.Printf("receive message from server: %s, exists: %v\n\n\n\n", reflect.TypeOf(m), d.msgHandlers[reflect.TypeOf(m)] != nil)
 		if handler, ok := d.msgHandlers[reflect.TypeOf(m)]; ok {
 			handler(m)
 		} else if d.defaultHandler != nil {
